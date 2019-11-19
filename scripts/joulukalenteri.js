20a3a03a7976ee21af5189787a490a26dd34a10b
Laurila Anni <bgk287@myy.haaga-helia.fi>
2019-11-19 13:41:28 +0200

    palautettu pvm-tarkistus

--- scripts/joulukalenteri.js
+++ scripts/joulukalenteri.js
@@ -2,8 +2,12 @@ const kuluvaPaiva = new Date();
 
 function avaaLuukku(luukkuNro) {
 
-    //näytetään luukkunumeron mukainen viesti
-    alert(luukkuNro + ". päivä: " + messages[luukkuNro - 1]);
+    //tarkistetaan onko luukku avattavissa
+    //HUOM! Tämä korjattava vielä lopulliseen versioon --> kk=11
+    if (kuluvaPaiva.getMonth()+1 < 10 || kuluvaPaiva.getDate() < luukkuNro) {
+        //näytetään luukkunumeron mukainen viesti
+        alert(luukkuNro + ". päivä: " + messages[luukkuNro - 1]);
+    }
           
 }
 
