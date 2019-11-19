224681362b2d1bb7d4b083ca2cdf6b5e8fbd6e35
Bergman Sebastian <bgk298@myy.haaga-helia.fi>
2019-11-19 14:03:01 +0200

    Merge

--- scripts/joulukalenteri.js
+++ scripts/joulukalenteri.js
@@ -1,12 +1,10 @@
 const kuluvaPaiva = new Date();
 
 function avaaLuukku(luukkuNro) {
-
-    //tarkistetaan onko luukku avattavissa
-    //HUOM! Tämä korjattava vielä lopulliseen versioon --> kk=11
-    if (kuluvaPaiva.getMonth()+1 < 10 || kuluvaPaiva.getDate() < luukkuNro) {
-        //näytetään luukkunumeron mukainen viesti
-        alert(luukkuNro + ". päivä: " + messages[luukkuNro - 1]);
-    }
-          
+    if (kuluvaPaiva.getMonth()+1 < 10 || kuluvaPaiva.getDate() >= luukkuNro) {
+    //näytetään luukkunumeron mukainen viesti
+    alert(luukkuNro + ". päivä: " + messages[luukkuNro - 1]);
+    }   
 }
+
+
