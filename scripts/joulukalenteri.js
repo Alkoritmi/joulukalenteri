b2e6c615f0792545679e72a1ac4d09212e68009d
Laurila Anni <bgk287@myy.haaga-helia.fi>
2019-11-19 14:02:25 +0200

    poistettu konsolilokitus

--- scripts/joulukalenteri.js
+++ scripts/joulukalenteri.js
@@ -4,8 +4,6 @@ function avaaLuukku(luukkuNro) {
 
     //tarkistetaan onko luukku avattavissa
     //HUOM! Tämä korjattava vielä lopulliseen versioon --> kk=11
-    console.log(kuluvaPaiva.getDate());
-    console.log(kuluvaPaiva.getMonth());
     if (kuluvaPaiva.getMonth() == 10 && kuluvaPaiva.getDate() >= luukkuNro) {
         //näytetään luukkunumeron mukainen viesti
         alert(luukkuNro + ". päivä: " + messages[luukkuNro - 1]);
