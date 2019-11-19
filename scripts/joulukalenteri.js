d0a5266a9e9f211085648c7b7ee1e27a70934f91
Laurila Anni <bgk287@myy.haaga-helia.fi>
2019-11-19 13:02:16 +0200

    korostus ja pvm korjaus

--- scripts/joulukalenteri.js
+++ scripts/joulukalenteri.js
@@ -3,22 +3,26 @@ const kuluvaPaiva = new Date();
 function avaaLuukku (luukkuNro) {
     
     //tarkistetaan onko luukku avattavissa
-    //HUOM! Tämä korjattava vielä lopulliseen versioon --> kk=12
-    if (kuluvaPaiva.getMonth()+1 < 11 || kuluvaPaiva.getDate() < luukkuNro) {
+    //HUOM! Tämä korjattava vielä lopulliseen versioon --> kk=11
+    if (kuluvaPaiva.getMonth()+1 < 10 || kuluvaPaiva.getDate() < luukkuNro) {
         
         //disabloidaan luukku
         poistakaytosta(luukkuNro);
         
     } else {
         //jos kuluva päivä, korostetaan luukku
-        if (kuluvaPaiva.getMonth == 12 && kuluvaPaiva.getDate == luukkuNro) {
+        //HUOM! Tämä korjattava vielä lopulliseen versioon --> kk=11
+        if (kuluvaPaiva.getMonth() == 10 && kuluvaPaiva.getDate() == luukkuNro) {
             //korostetaan luukku
+            document.getElementById(luukkuNro).classList.remove("luukku");
+            document.getElementById(luukkuNro).classList.add("luukku_korostus");
+            
         }
           
         
         //näytetään luukkunumeron mukainen viesti
         alert(luukkuNro + ". päivä: " + messages[luukkuNro - 1]);
-        
+          
     }
     
     
